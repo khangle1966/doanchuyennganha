d0f356c3cfb937a41466e91bb6c70b4d7ce5e803
PhKhoa <khoa.pm14576@sinhvien.hoasen.edu.vn>
2023-08-27 02:03:29 +0700

    code trang home + server lessons

--- server/src/lessons/lessons.module.ts
+++ server/src/lessons/lessons.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { LessonsService } from './lessons.service';
 import { LessonsController } from './lessons.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Lesson,LessonSchema } from './entities/lesson.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Lesson.name, schema: LessonSchema }
+    ]),
+  ],
   controllers: [LessonsController],
   providers: [LessonsService],
 })
--- server/src/lessons/lessons.service.ts
+++ server/src/lessons/lessons.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateLessonDto } from './dto/create-lesson.dto';
 import { UpdateLessonDto } from './dto/update-lesson.dto';
+import { Lesson } from './entities/lesson.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class LessonsService {
-  create(createLessonDto: CreateLessonDto) {
-    return 'This action adds a new lesson';
+  createLesson(createLessonDto: CreateLessonDto) {
+    throw new Error('Method not implemented.');
+  }
+  constructor(@InjectModel(Lesson.name) private lessonModel: Model<Lesson>){}
+
+  async create(createLessonDto: CreateLessonDto): Promise<Lesson> {
+    try {
+      const newLesson = new this.lessonModel({
+        ...createLessonDto,
+      });
+      return await newLesson.save();
+    } catch (error) {
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all lessons`;
+  async getById(id: string): Promise<Lesson> {
+    try{
+      return await this.lessonModel.findById(id).exec();
+    }catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} lesson`;
+  async getAll(): Promise<Lesson[]> {
+    try {
+      return await this.lessonModel.find().exec();
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
   }
 
-  update(id: number, updateLessonDto: UpdateLessonDto) {
-    return `This action updates a #${id} lesson`;
+  async update(id: string, updateLessonDto: UpdateLessonDto): Promise<Lesson> {
+    try{
+      return this.lessonModel.findByIdAndUpdate(
+        {_id:id},
+        {...updateLessonDto}, 
+        { new: true });
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} lesson`;
+  async delete(id: string): Promise<Lesson> {
+    try {
+      const deletedLesson = await this.lessonModel.findByIdAndDelete(id);
+      return deletedLesson;
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
   }
+
+
 }
