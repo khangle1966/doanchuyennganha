c24b8c64c227acc4a25ba91401582a365a1e6ba6
Honekawa <manhvp1810@gmail.com>
2023-08-27 01:21:56 +0700

    add editor library to app

--- Client/src/app/pages/layout/components/admin/components/lesson/lesson.component.ts
+++ Client/src/app/pages/layout/components/admin/components/lesson/lesson.component.ts
@@ -7,100 +7,37 @@ import { Lesson } from 'src/app/models/Lesson.model';
   styleUrls: ['./lesson.component.less'],
 })
 export class LessonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
   lessonList: Lesson[] = [
     {
       _id: '1',
-      title: 'Lesson 1',
-      content: 'Lesson 1 content',
-      imageUrl: 'https://picsum.photos/200/300',
-      courseId: '1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      _id: '2',
-      title: 'Lesson 2',
-      content: 'Lesson 2 content',
-      imageUrl: 'https://picsum.photos/200/300',
-      courseId: '1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      _id: '3',
-      title: 'Lesson 3',
-      content: 'Lesson 3 content',
-      imageUrl: 'https://picsum.photos/200/300',
-      courseId: '1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      _id: '4',
-      title: 'Lesson 4',
-      content: 'Lesson 4 content',
-      imageUrl: 'https://picsum.photos/200/300',
-      courseId: '1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      _id: '5',
-      title: 'Lesson 5',
-      content: 'Lesson 5 content',
-      imageUrl: 'https://picsum.photos/200/300',
-      courseId: '1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      _id: '6',
-      title: 'Lesson 6',
-      content: 'Lesson 6 content',
-      imageUrl: 'https://picsum.photos/200/300',
-      courseId: '1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      _id: '7',
-      title: 'Lesson 7',
-      content: 'Lesson 7 content',
-      imageUrl: 'https://picsum.photos/200/300',
-      courseId: '1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      _id: '8',
-      title: 'Lesson 8',
-      content: 'Lesson 8 content',
-      imageUrl: 'https://picsum.photos/200/300',
-      courseId: '1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      _id: '9',
-      title: 'Lesson 9',
-      content: 'Lesson 9 content',
-      imageUrl: 'https://picsum.photos/200/300',
-      courseId: '1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      _id: '10',
-      title: 'Lesson 10',
-      content: 'Lesson 10 content',
-      imageUrl: 'https://picsum.photos/200/300',
+      title: 'Lesson ' + Date.now().toString(),
+      content: 'Lesson ' + Date.now().toString() + ' content',
+      imageUrl: '../../../../../../../assets/images/lisa.jpg',
       courseId: '1',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
   ];
 
-  constructor() {}
+  selectedLesson: Lesson | null = null;
 
-  ngOnInit(): void {}
+  addLesson() {
+    this.lessonList.push({
+      _id: Date.now().toString(),
+      title: 'Lesson ' + Date.now().toString(),
+      content: 'Lesson ' + Date.now().toString() + ' content',
+      imageUrl: '../../../../../../../assets/images/lisa.jpg',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    });
+  }
+
+  selectLesson(lesson: Lesson) {
+    this.selectedLesson = lesson;
+  }
 }
