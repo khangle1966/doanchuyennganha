51ef1bf157ba033b4fd8af3f40e482a0b57e89c8
Honekawa <manhvp1810@gmail.com>
2023-08-26 18:37:31 +0700

    create lesson page and edit lesson

--- Client/src/app/pages/layout/components/admin/components/lesson/lesson.component.ts
+++ Client/src/app/pages/layout/components/admin/components/lesson/lesson.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Lesson } from 'src/app/models/Lesson.model';
 
 @Component({
   selector: 'app-lesson',
@@ -6,6 +7,99 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lesson.component.less'],
 })
 export class LessonComponent implements OnInit {
+  lessonList: Lesson[] = [
+    {
+      _id: '1',
+      title: 'Lesson 1',
+      content: 'Lesson 1 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      _id: '2',
+      title: 'Lesson 2',
+      content: 'Lesson 2 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      _id: '3',
+      title: 'Lesson 3',
+      content: 'Lesson 3 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      _id: '4',
+      title: 'Lesson 4',
+      content: 'Lesson 4 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      _id: '5',
+      title: 'Lesson 5',
+      content: 'Lesson 5 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      _id: '6',
+      title: 'Lesson 6',
+      content: 'Lesson 6 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      _id: '7',
+      title: 'Lesson 7',
+      content: 'Lesson 7 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      _id: '8',
+      title: 'Lesson 8',
+      content: 'Lesson 8 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      _id: '9',
+      title: 'Lesson 9',
+      content: 'Lesson 9 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      _id: '10',
+      title: 'Lesson 10',
+      content: 'Lesson 10 content',
+      imageUrl: 'https://picsum.photos/200/300',
+      courseId: '1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
