0e3ea5c5cf3079752ee0e1590b518cf6792fb491
Meoneff <hahoangduy95@gmail.com>
2023-09-01 23:47:10 +0700

    conflit

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -27,7 +27,9 @@ import { profileReducer } from './ngrx/reducers/profile.reducer';
 import { UserEffects } from './ngrx/effects/user.effects';
 import { ProfileEffects } from './ngrx/effects/profile.effects';
 import { HttpClientModule } from '@angular/common/http';
-
+import { CourseReducer } from './ngrx/reducers/course.reducer';
+import { CartReducer } from './ngrx/reducers/cart.reducer';
+import { CourseEffect } from './ngrx/effects/course.effects';
 @NgModule({
   declarations: [AppComponent, LoadingComponent],
   imports: [
@@ -36,10 +38,21 @@ import { HttpClientModule } from '@angular/common/http';
     // HttpModule,
     AppRoutingModule,
     StoreModule.forRoot(
-      { auth: authReducer, user: userReducer, profile: profileReducer },
+      {
+        auth: authReducer,
+        user: userReducer,
+        profile: profileReducer,
+        course: CourseReducer,
+        cart: CartReducer,
+      },
       {}
     ),
-    EffectsModule.forRoot([AuthEffects, UserEffects, ProfileEffects]),
+    EffectsModule.forRoot([
+      AuthEffects,
+      UserEffects,
+      ProfileEffects,
+      CourseEffect,
+    ]),
     BrowserAnimationsModule,
     HttpClientModule,
     TuiRootModule,
