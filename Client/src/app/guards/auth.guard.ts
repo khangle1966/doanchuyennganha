44b9755d4c8265673c5204d0256d0db181c90ce5
Honekawa <manhvp1810@gmail.com>
2023-09-03 01:49:49 +0700

    add auth guard for block invalid auth,add lineclamp for course description

---
+++ Client/src/app/guards/auth.guard.ts
@@ -0,0 +1,56 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable, catchError, map, of } from 'rxjs';
+import { UserState } from '../ngrx/states/user.state';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PermissionsService {
+  constructor(
+    private store: Store<{ user: UserState }>,
+    private router: Router
+  ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.store.select('user', 'user').pipe(
+      map((user) => {
+        // console.log(user);
+        if (user.uid != '' && user.uid != undefined && user.uid != null) {
+          return true;
+        } else {
+          this.router.navigateByUrl('/welcome');
+          return false;
+        }
+      }),
+      catchError(() => {
+        this.router.navigateByUrl('/welcome');
+        return of(false);
+      })
+    );
+  }
+}
+
+export const AuthGuard: CanActivateFn = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+):
+  | Observable<boolean | UrlTree>
+  | Promise<boolean | UrlTree>
+  | boolean
+  | UrlTree => {
+  return inject(PermissionsService).canActivate(next, state);
+};
